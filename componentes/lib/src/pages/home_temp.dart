92d33c07f20568c5e55673970ed761858c107bdd
Alberto Chable <alberto.chable@icami.mx>
2020-11-01 19:18:00 -0600

    ListTile completado con lista de elementos en código

--- componentes/lib/src/pages/home_temp.dart
+++ componentes/lib/src/pages/home_temp.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno','Dos','Tres','Cuatro','Cinco','Seis','Siete','Ocho','Nueve','Diez'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,20 +9,49 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Componentes Temp')
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('ListTile title')
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile title')
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile title')
-          )
-        ]
+        children: _crearItemsCorto()
       )
     );
   }
+/*
+  List <Widget> _crearItems() {
+    List <Widget> lista = new List <Widget>();
+    
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title:  Text(opt)
+      );
+
+      // lista.add(tempWidget);
+      // lista.add(Divider(color: Color.fromARGB(50, 34, 139, 34), height: 20.0,));
+      //Operador de cascada usa el doble punto .. para mantener la referencia
+      //la instrucción no se cierra con punto y coma. Se escribe la siguiente instrucción.
+      lista..add(tempWidget)
+           ..add(Divider(color: Color.fromARGB(50, 34, 139, 34), height: 20.0,));
+    }
+
+    return lista;
+  }
+*/
+
+  List <Widget> _crearItemsCorto() {
+
+    var widgets = opciones.map( (elemento) {
+      return Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.account_balance_wallet),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            title: Text(elemento + '!'),
+            subtitle: Text('Subtitulo ' + elemento),
+            onTap: (){}
+          ),
+          Divider(color: Color.fromARGB(50, 34, 139, 34), height: 50.0)
+        ],
+      );
+    }).toList();
+
+    return widgets;
+  }
+
 }
